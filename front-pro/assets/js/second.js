c1ec31db37aacfd09eac77234e1fae200d18708c
John Doe <leylaam@code.edu.az>
2020-06-18 17:36:23 +0400

    commit

--- front-pro/assets/js/second.js
+++ front-pro/assets/js/second.js
@@ -2,81 +2,82 @@
 
 let pattern=/(.*)\/(.*)/g;
 $(document).ready(function () {
-    $.ajax({
-        async:false,
-        type: "GET",
-        url: "http://kamranaeff1994-002-site3.ctempurl.com/api/Doctors",
-        beforeSend: function() {
+    // $.ajax({
+    //     async:false,
+    //     type: "GET",
+    //     url: "http://kamranaeff1994-002-site3.ctempurl.com/api/Doctors",
+    //     beforeSend: function() {
             
-            $('.loader').addClass('show');
-        },
-        success: function (response) {
-            $.each(response, function (indexInArray, item) { 
+    //         $('.loader').addClass('show');
+    //     },
+    //     success: function (response) {
+    //         $.each(response, function (indexInArray, item) { 
                
                     
-     let allCards=$(`<div class="card item" ><div class="image1">
-                        <div class="overlay">
-                        <ul class="list d-flex flex-row">
+    //  let allCards=$(`<div class="card item" ><div class="image1">
+    //                     <div class="overlay">
+    //                     <ul class="list d-flex flex-row">
                        
-                        <li>
-                        <a href="#"><i class="fab mr-3 fa-instagram mr-3"></i></a></li>
-                       <li><a href="#"><i class="fab fa-facebook mr-3"></i></a></li>
-                       <li><a href="#"><i class="fab fa-twitter mr-3"></i></a></li>
+    //                     <li>
+    //                     <a href="#"><i class="fab mr-3 fa-instagram mr-3"></i></a></li>
+    //                    <li><a href="#"><i class="fab fa-facebook mr-3"></i></a></li>
+    //                    <li><a href="#"><i class="fab fa-twitter mr-3"></i></a></li>
                        
-                       </ul>
-                       </div>
-                        <img class="card-img-top" src="http://kamranaeff1994-002-site3.ctempurl.com/images/${item.image}" alt="Card image cap">   
-                        </div>
-                        <div class="card-body">
-                          <h5 class="card-title text-center">${item.name}</h5>
-                          <h6 class="doc-title text-center">
-                           ${item.profession}
-                          </h6>
-                          <p class="card-text text-center pb-3">${item.description}</p>
+    //                    </ul>
+    //                    </div>
+    //                     <img class="card-img-top" src="http://kamranaeff1994-002-site3.ctempurl.com/images/${item.image}" alt="Card image cap">   
+    //                     </div>
+    //                     <div class="card-body">
+    //                       <h5 class="card-title text-center">${item.name}</h5>
+    //                       <h6 class="doc-title text-center">
+    //                        ${item.profession}
+    //                       </h6>
+    //                       <p class="card-text text-center pb-3">${item.description}</p>
                         
-                        </div>
-                      </div>`);
+    //                     </div>
+    //                   </div>`);
                     
-                    // $(col).html(allCards)
-                    $('.products').append(allCards);    
-            });
+    //                 // $(col).html(allCards)
+    //                 $('.products').append(allCards);    
+    //         });
          
     
-    }  
-    }).done(function(){
-        $('.loader').fadeOut(1000,function(){
+    // }  
+    // }).done(function(){
+    //     $('.loader').fadeOut(1000,function(){
         
-         $('.loader').removeClass('show');
-    });  
-        $('.owl-carousel').owlCarousel({
-            loop:true,
-            margin:10,
-            autoplay:true,
-            navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    //      $('.loader').removeClass('show');
+    // });  
+    //     $('.owl-carousel').owlCarousel({
+    //         loop:true,
+    //         margin:10,
+    //         autoplay:true,
+    //         navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
     
-            autoplayTimeout:4000,
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items:1,
-                    nav:true
-                },
-                600:{
-                    margin:10,
-                    items:2,
-                    nav:false
-                },
-                1000:{
-                    items:3,
-                    nav:true,
+    //         autoplayTimeout:4000,
+    //         responsiveClass:true,
+    //         responsive:{
+    //             0:{
+    //                 items:1,
+    //                 nav:true
+    //             },
+    //             600:{
+    //                 margin:10,
+    //                 items:2,
+    //                 nav:false
+    //             },
+    //             1000:{
+    //                 items:3,
+    //                 nav:true,
                    
-                }
-            }
-        })
+    //             }
+    //         }
+    //     })
  
-     })
+    //  })
    
     $(function () {
+      
         $('.header2').load("_header.html",function(){
 let pathName=window.location.pathname;
 console.log(pathName)
@@ -129,6 +130,7 @@ $('.rightDoub').click(function (e) {
     
     
     
+
     setInterval(() => { 
       
          
@@ -153,6 +155,36 @@ $('.rightDoub').click(function (e) {
     }, 5000);
     
    
+    $('.formAppoint').validate({
+        errorPlacement: function(error, element) {
+            $(error).addClass('invalid-feedback text-white');
+            error.insertAfter(element)
+        },
+
+        hightlight:function(element,errorClass){
+            $(element).addClass('is-invalid text-white')
+            
+        },
+
+
+        unhightlight:function(element,errorClass){
+            $(element).removeClass('is-invalid')
+            .closest('form')
+          addClass('was-validated')
+        }
+
+    })
+
+    $('.formAppoint').submit(function (e) { 
+        e.preventDefault();
+        console.log('e.currentTarget.value')
+    });
+
+
+
+
+
+
     window.addEventListener('scroll', function () {
         let scroll = document.querySelector('.scroll')
         let ust = document.querySelector('.navbar')
@@ -160,7 +192,7 @@ $('.rightDoub').click(function (e) {
 
         if (a > 50) {
             scroll.classList.add('show', 'text-white')
-            ust.classList.add('pos')
+           ust.classList.add('pos')
         }
         else {
             scroll.classList.remove('show')
@@ -242,4 +274,4 @@ updateCount();
 
 
 
-});
+})
